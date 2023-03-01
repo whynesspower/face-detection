75237bd53462d7b56deaf847ce4a5d64ab219952
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 20:09:10 +0530

    Adding Component: InsertLinkButton

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLink from "./components/ImageLink/ImageLink";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm /> */}
+      <ImageLink />
     </div>
   );
 }
