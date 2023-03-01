c0a4302fa3b90b7e4e260159806434a92843dfc1
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 19:08:32 +0530

    Enhacing Navigation components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Logo />
-      <ImageLinkForm />
+      {/* <Logo /> */}
+      {/* <ImageLinkForm /> */}
     </div>
   );
 }
