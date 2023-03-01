834a9486d1243114631d94d2ef4d5c6df19f20c8
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 20:15:56 +0530

    Adding Component: InsertLinkButton CSS

--- my-app/src/components/ImageLink/ImageLink.js
+++ my-app/src/components/ImageLink/ImageLink.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./ImageLink.css";
 const ImageLink = () => {
   return (
     <div>
@@ -8,11 +8,13 @@ const ImageLink = () => {
           "This Magic Brain will detect Face in your given Picture. Give it a try!"
         }
       </p>
-      <div className="display">
-        <input className="f4 pa2 w-70 center" type="tex" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-          Detect
-        </button>
+      <div className="center">
+        <div className="form pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="tex" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
       </div>
     </div>
   );
