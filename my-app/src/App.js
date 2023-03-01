89799d8c75f0570074eea875c92928f41b254f2d
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 20:22:55 +0530

    Adding Component Rank Button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLink from "./components/ImageLink/ImageLink";
-
+import Rank from "./components/Rank/Rank";
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
+
       <ImageLink />
     </div>
   );
