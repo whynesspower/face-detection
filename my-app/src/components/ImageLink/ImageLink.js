9af06212f0fde3f87f2fe1be0c12d828b28b6e57
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 22:04:41 +0530

    Adding Functionality to the button

--- my-app/src/components/ImageLink/ImageLink.js
+++ my-app/src/components/ImageLink/ImageLink.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ImageLink.css";
-const ImageLink = () => {
+const ImageLink = ({ onInputChange }) => {
   return (
     <div>
       <p className="f3">
@@ -10,7 +10,11 @@ const ImageLink = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="tex" />
+          <input
+            className="f4 pa2 w-70 center"
+            type="tex"
+            onChange={onInputChange}
+          />
           <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
           </button>
