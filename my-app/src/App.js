6203fdd2d90bf6b5ba1b6a8664dbcb1b0bed997f
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-01 22:04:37 +0530

    Adding Functionality to the button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,15 +5,18 @@ import Logo from "./components/Logo/Logo";
 import ImageLink from "./components/ImageLink/ImageLink";
 import Rank from "./components/Rank/Rank";
 // import Particles from "react-particles-js";
+function onInputChange(event) {
+  console.log(event.target.value);
+}
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Logo />
       <Rank />
+      <Logo />
 
-      <ImageLink />
+      <ImageLink onInputChange={onInputChange} />
     </div>
   );
 }
